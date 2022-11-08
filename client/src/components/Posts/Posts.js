47494a7da20db06e59ea963e74642aa7a035f895
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2022-11-08 15:06:09 +0200

    create store, actions, reducers for Posts

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Post from './Post/Post';
+
+import useStyles from './styles';
+
+const Posts = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
