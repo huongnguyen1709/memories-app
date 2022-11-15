11fcfeffe0bfb74ddd7b09e34d1d6566c9113481
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2022-11-15 18:44:41 +0200

    feat(server): add new route likePost to the server

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,3 +45,21 @@ export const deletePost = async (req, res) => {
   await PostMessage.findByIdAndRemove(_id);
   res.json({ message: 'Post deleted successfully' });
 };
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send('No post with that id');
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    {
+      likeCount: post.likeCount + 1,
+    },
+    { new: true }
+  );
+
+  res.json(updatedPost);
+};
